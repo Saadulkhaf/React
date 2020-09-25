563ee05c0db6684568c35842da824b51b8a5a962
saadulkhaf <saadafzal995@gmail.com>
2020-09-26 01:44:56 +0500

    tytyt

--- ajx/src/ComponentDetail.js
+++ ajx/src/ComponentDetail.js
@@ -12,9 +12,9 @@ const ComponentDetail = (props) => {
           {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
   );
--- ajx/src/index.js
+++ ajx/src/index.js
@@ -5,9 +5,21 @@ import ComponentDetail from "./ComponentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ComponentDetail author="Sam" />
-      <ComponentDetail author="Alex" />
-      <ComponentDetail author="Jane" />
+      <ComponentDetail
+        author="Sam"
+        timeAgo="Today at 5:00AM"
+        content="Nice blogpost"
+      />
+      <ComponentDetail
+        author="Alex"
+        timeAgo="Today at 8:00AM"
+        content="Good blogpost"
+      />
+      <ComponentDetail
+        author="Jane"
+        timeAgo="Today at 1:00AM"
+        content="Bad blogpost"
+      />
     </div>
   );
 };
