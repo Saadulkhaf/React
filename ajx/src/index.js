7dd08d4bafcf53e7ee39a9f4b1620454855d4ada
saadulkhaf <saadafzal995@gmail.com>
2020-09-25 22:53:08 +0500

    Reuseable component with props

--- ajx/src/index.js
+++ ajx/src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ComponentDetail from "./ComponentDetail";
 
 const App = () => {
-  return <div>HI There</div>;
+  return (
+    <div className="ui container comments">
+      <ComponentDetail author="Sam" />
+      <ComponentDetail author="Alex" />
+      <ComponentDetail author="Jane" />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
