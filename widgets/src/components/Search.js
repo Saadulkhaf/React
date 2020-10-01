503b11818df1b54e6d488e7013bb1857308dbae0
saadulkhaf <saadafzal995@gmail.com>
2020-10-01 14:19:04 +0500

    controlled input using useState

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  const [term, setTerm] = useState("");
+  console.log(term);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
