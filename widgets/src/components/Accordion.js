37fbb2eed311770051cce9dbe90eb74db7fdda33
saadulkhaf <saadafzal995@gmail.com>
2020-10-01 12:56:16 +0500

    started implementing accordion

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-const Accordion = (props) => {
-  return (
-    <div>
-      <h1>{props.items.length}</h1>
-    </div>
-  );
+const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div className="active title">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
